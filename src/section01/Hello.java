8745527395caa9237e48f1d43a2dc183cd81318b
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-04-23 14:37:35 +0900

    first commit

---
+++ src/section01/Hello.java
@@ -0,0 +1,21 @@
+package section01;
+/* 여러줄 주석
+ * 
+ * 
+ */
+public class Hello {
+	public static void main(String[] args) {
+		// 한줄 주석
+		System.out.println("Hello, JAVA!");
+		
+		}
+	/** JavaDoc  주석
+	 * 
+	 * printNum - 입력받은 정수 출력
+	 * @param num
+	 */
+	public static void prinNum(int num) {
+		System.out.println("num:" + num);
+	}
+	
+}
